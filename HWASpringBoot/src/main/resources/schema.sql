fd7411fb5dea3198a7f61f367780aabf6c533baf
gabejose <g.jose10xx@gmail.com>
2021-11-17 14:28:54 +0000

    fixed schema.sql

--- HWASpringBoot/src/main/resources/schema.sql
+++ HWASpringBoot/src/main/resources/schema.sql
@@ -1,23 +1,23 @@
-DROP TABLE IF EXISTS boatclubs;
+DROP TABLE IF EXISTS boat_clubs;
 DROP TABLE IF EXISTS boats;
 
-CREATE TABLE IF NOT EXISTS boat_clubs (
+CREATE TABLE boat_clubs (
 	boat_club_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	boat_club_name NOT NULL VARCHAR(255),
-	address NOT NULL VARCHAR(255),
-	boat_club_code NOT NULL CHAR(3),
-	head_coach_first_name NOT NULL VARCHAR(255),
-	head_coach_surname NOT NULL VARCHAR(255)
+	boat_club_name VARCHAR(255) NOT NULL,
+	address VARCHAR(255) NOT NULL,
+	boat_club_code CHAR(3) NOT NULL,
+	head_coach_first_name VARCHAR(255) NOT NULL,
+	head_coach_surname VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS boats (
+CREATE TABLE boats (
 	boat_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	boat_type NOT NULL VARCHAR(3),
-	boat_company NOT NULL VARCHAR(255),
-	avg_crew_weight NOT NULL INT,
-	rigger_material NOT NULL VARCHAR(255),
-	is_wing_rigger NOT NULL CHAR(1),
-	boat_club_id INT NOT NULL
-	FOREIGN KEY (boat_club_id) REFERENCES boat_clubs (boat_club_id)
+	boat_type VARCHAR(3) NOT NULL,
+	boat_company VARCHAR(255) NOT NULL,
+	avg_crew_weight INT NOT NULL,
+	rigger_material VARCHAR(255) NOT NULL,
+	is_wing_rigger CHAR(1) NOT NULL,
+	boat_club_id INT NOT NULL,
+	FOREIGN KEY (boat_club_id) REFERENCES boat_clubs (boat_club_id),
 	ON DELETE CASCADE
 );
