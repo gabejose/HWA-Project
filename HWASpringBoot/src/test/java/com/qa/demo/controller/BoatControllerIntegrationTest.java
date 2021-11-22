d992f84449fccbae4df5f378e091baf744711f67
gabejose <g.jose10xx@gmail.com>
2021-11-22 14:42:47 +0000

    added integration tests for boat controller

--- HWASpringBoot/src/test/java/com/qa/demo/controller/BoatControllerIntegrationTest.java
+++ HWASpringBoot/src/test/java/com/qa/demo/controller/BoatControllerIntegrationTest.java
@@ -1,8 +1,13 @@
 package com.qa.demo.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +37,7 @@ public class BoatControllerIntegrationTest {
 	
 	@Test
 	public void createBoatTest() throws Exception {
-		BoatClub club = new BoatClub("London Rowing Club", "123 Anystreet Putney Embankment SW10 9HF", "LRC", "Stuart", "Heap");
+		BoatClub club = new BoatClub(1L, "London Rowing Club", "123 Anystreet Putney Embankment SW10 9HF", "LRC", "Stuart", "Heap");
 		Boat boat = new Boat("8+", "Empacher", 95, "Carbon", true, club);
 		
 		String boatAsJSON = this.mapper.writeValueAsString(boat);
@@ -49,22 +54,50 @@ public class BoatControllerIntegrationTest {
 	}
 	
 	@Test
-	public void getAllBoatTest() {
+	public void getAllBoatsTest() throws Exception {
+		BoatClub club = new BoatClub(1L, "London Rowing Club", "123 Anystreet Putney Embankment SW10 9HF", "LRC", "Stuart", "Heap");
+		Boat boat = new Boat(1L, "8+", "Empacher", 95, "Carbon", true, club);
+		String boatAsJSON = this.mapper.writeValueAsString(List.of(boat));
+		RequestBuilder request = get("/boat/getAllBoats").contentType(MediaType.APPLICATION_JSON).content(boatAsJSON);
 		
+		ResultMatcher checkStatus = status().isAccepted();
+		ResultMatcher checkBody = content().json(boatAsJSON);
+		
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
 	}
 	
 	@Test
-	public void getBoatByIdTest() {
+	public void getBoatByIdTest() throws Exception {
+		BoatClub club = new BoatClub(1L, "London Rowing Club", "123 Anystreet Putney Embankment SW10 9HF", "LRC", "Stuart", "Heap");
+		Boat boat = new Boat(1L, "8+", "Empacher", 95, "Carbon", true, club);
+		String boatAsJSON = this.mapper.writeValueAsString(boat);
+		RequestBuilder request = get("/boat/getBoatById/1").contentType(MediaType.APPLICATION_JSON);
 		
+		ResultMatcher checkStatus = status().isOk();
+		
+		ResultMatcher checkBody = content().json(boatAsJSON);
+		
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
 	}
 	
 	@Test
-	public void updateBoatTest() {
+	public void updateBoatTest() throws Exception {
+		BoatClub club = new BoatClub(1L, "London Rowing Club", "123 Anystreet Putney Embankment SW10 9HF", "LRC", "Stuart", "Heap");
+		Boat boat = new Boat(1L, "4x-", "Filippi", 82, "Aluminium", true, club);
+		String boatAsJSON = this.mapper.writeValueAsString(boat);
+		RequestBuilder request = put("/boat/updateBoat/1").contentType(MediaType.APPLICATION_JSON).content(boatAsJSON);
 		
+		ResultMatcher checkStatus = status().isAccepted();
+		ResultMatcher checkBody = content().json(boatAsJSON);
+		
+		this.mvc.perform(request).andExpect(checkStatus).andExpect(checkBody);
 	}
 	
 	@Test
-	public void deleteBoatTest() {
+	public void deleteBoatTest() throws Exception {
+		RequestBuilder request = delete("/boat/deleteBoat/1");
+		ResultMatcher checkStatus = status().isNoContent();
 		
+		this.mvc.perform(request).andExpect(checkStatus);
 	}
 }
